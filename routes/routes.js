dec194fc5b3f69ca333da0d9109552ace36e60ef
Iury Handric <handric11@hotmail.com>
2025-06-03 16:03:00 -0300

    Inserindo valores no banco de dados

--- routes/routes.js
+++ routes/routes.js
@@ -8,4 +8,28 @@ router.get('/', (req, res) => {
     res.render('home')
 })
 
+router.get('/users/create', (req,res) => {
+    res.render('adduser')
+})
+
+router.post('/users/create', async (req, res) => {
+    const name = req.body.name
+    const occupation = req.body.occupation
+    let newsletter = req.body.newsletter
+
+    if(newsletter === 'on') {
+        newsletter = true
+    } else {
+        newsletter = false
+    }
+
+    await User.create({name, occupation, newsletter})
+
+    console.log('Dados recebidos:', req.body)
+    console.log('Tipo de name:', typeof req.body.name)
+
+    res.redirect('/')
+
+})
+
 module.exports = router;
